d18d68e47f5bbcc6b1abcf3ae11358899b8e15e0
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-01-27 12:47:55 -0500

    new build
    added mongoose and pushed hardcoded data to a mongodb database, established a form for /new, can console log form data after hitting submit button

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
+//jshint esversion:6
+
 var express = require('express');
 var router = express.Router();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://cdhprof:Lyr1c%40%40%40@tafel-dev.goaul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+const messageSchema = new mongoose.Schema({
+  username: String,
+  message: String
+});
+
+const Message = mongoose.model("message", messageSchema);
+
+let mess = new Message({
+  username: "LuckyZeeland",
+  message: "Let's see if this works!"
+});
+
+mess.save();//-------THIS SUCCESSFULLY SAVES A MESSAGE TO THE DATABASE!!!!!!!
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get("/new", function(req, res, next) {
+  res.render("new", {title: "New Message"});
+});
+
+router.post("/new", function(req, res, next) {
+  console.log(req.body.username);
+  console.log(req.body.message);
+  res.redirect("/")
 });
 
 module.exports = router;
